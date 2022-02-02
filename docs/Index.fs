3e140d51c9d801c89465cba1c5ab7a8e85f648c2
Hsin <chenghsin42@gmail.com>
2022-02-02 18:45:49 +0100

    use Auth0 in doc web app (#4)
    * reference src project
    
    * wrap app in Auth0Provider
    
    * add auth0 login button
    
    * show avatar and username
    
    * logout
    
    * brief usage box setup
    
    * logout tooltip
    
    * right align icons in appbar
    
    * update favicon

--- docs/Index.fs
+++ docs/Index.fs
@@ -17,28 +17,105 @@ let update (msg: Msg) (model: Model) : Model =
     | AddTodo ->
         model
 
+open Fable.Core
+open Fable.Auth0.React
 open Feliz
 open Feliz.MaterialUI
 module Mdi = Fable.MaterialUI.Icons
+
+let auth0App (children: seq<ReactElement>) =
+    let opts =
+        unbox<Auth0ProviderOptions>
+            {| domain = "dev-nik3xlx8.us.auth0.com"
+               clientId = "sGGwICcD2Cnp3DX1A0kacQmcsY0Ri7nu"
+               redirectUri = "http://localhost:8080" |}
+    Auth0Provider opts children
+
+[<ReactComponent>]
+let ProfileBox () =
+    let ctxAuth0 = useAuth0 ()
+    let handleLoginWithRedirect _ =
+        let opts = unbox<RedirectLoginOptions> null
+
+        ctxAuth0.loginWithRedirect opts
+        |> Async.AwaitPromise
+        |> Async.StartImmediate
+    let handleLogoutWithRedirect _ =
+        let returnTo = Browser.Dom.window.location.origin
+        let opts = unbox<LogoutOptions> {| returnTo = returnTo |}
+        ctxAuth0.logout opts
+
+    if not ctxAuth0.isAuthenticated then
+        Mui.button [
+            button.color.secondary
+            button.variant.contained
+            prop.onClick handleLoginWithRedirect
+            prop.text "Login with Auth0"
+        ]
+    else
+        let username, picture =
+            match ctxAuth0.user with
+            | Some u ->
+                sprintf "%A" u.name,
+                sprintf "%A" u.picture
+            | None -> "", ""
+        Mui.tooltip [
+            tooltip.title "Click to logout"
+            tooltip.arrow true
+            tooltip.children (
+                Mui.button [
+                    button.color.inherit'
+                    button.size.large
+                    button.startIcon (
+                        Mui.avatar [
+                            avatar.alt username
+                            avatar.src picture
+                        ]
+                    )
+                    button.endIcon (
+                        Mdi.exitToAppIcon [ ]
+                    )
+                    button.children (
+                        Html.h4 username
+                    )
+                    prop.onClick handleLogoutWithRedirect
+                ]
+            )
+        ]
+
 
 let topBar =
     Mui.container [
         Mui.toolbar [
             toolbar.variant.dense
             toolbar.children [
-                Html.h1 "Fable.Auth0.React"
+                Html.div [
+                    prop.style [ style.flexGrow 1 ]
+                    prop.children [
+                        Html.h1 "Fable.Auth0.React"
+                    ]
+                ]
                 Mui.button [
                     button.href "https://github.com/chengh42/Fable.Auth0.React"
                     button.size.large
                     button.color.inherit'
                     prop.children [ Mdi.gitHubIcon [] ]
                 ]
+                ProfileBox ()
             ]
         ]
     ]
 
+[<ReactComponent>]
+let UsageBox () =
+    Mui.container [
+        Html.h2 "Usage"
+        Html.p "WIP, coming soon."
+    ]
+
 let view (model: Model) (dispatch: Msg -> unit) =
-    Html.div [
+
+    auth0App [
         Mui.appBar [
             appBar.color.primary
             appBar.position.sticky
@@ -57,12 +134,7 @@ let view (model: Model) (dispatch: Msg -> unit) =
                     ]
                     Html.span ", the Auth0 SDK for React Single Page Applications (SPA)."
                 ]
-                Html.p "Note: Still a work-in-progress!"
-                Mui.button [
-                    button.color.primary
-                    button.variant.contained
-                    prop.text "Hello"
-                ]
+                UsageBox ()
             ]
         ]
     ]
