68651d3e5e486940200c522b2621a66e270026a9
Hsin <hsin@impactinstitute.com>
2022-02-02 16:07:57 +0100

    use material ui

--- docs/Index.fs
+++ docs/Index.fs
@@ -18,12 +18,27 @@ let update (msg: Msg) (model: Model) : Model =
         model
 
 open Feliz
-open Feliz.DaisyUI
+open Feliz.MaterialUI
 
 let view (model: Model) (dispatch: Msg -> unit) =
-    Html.div [
-        Daisy.button.a [
-            color.bgPrimary
-            prop.text "Hello there"
+    Mui.container [
+        container.maxWidth.lg
+        container.children [
+            Html.h1 "Fable.Auth0.React"
+            Html.p [
+                Html.span "Fable library for "
+                Html.a [
+                    prop.href "https://github.com/auth0/auth0-react"
+                    prop.target "_blank"
+                    prop.children [ Html.span "@auth0/auth0-react" ]
+                ]
+                Html.span ", the Auth0 SDK for React Single Page Applications (SPA)."
+            ]
+            Html.p "Note: Still a work-in-progress!"
+            Mui.button [
+                button.color.primary
+                button.variant.contained
+                prop.text "Hello"
+            ]
         ]
     ]
