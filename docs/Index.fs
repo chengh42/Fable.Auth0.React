20fd530e955e528eebcf2f18222956ce5d16d89b
Hsin <hsin@impactinstitute.com>
2022-02-02 16:45:09 +0100

    add contents

--- docs/Index.fs
+++ docs/Index.fs
@@ -19,26 +19,50 @@ let update (msg: Msg) (model: Model) : Model =
 
 open Feliz
 open Feliz.MaterialUI
+module Mdi = Fable.MaterialUI.Icons
+
+let topBar =
+    Mui.container [
+        Mui.toolbar [
+            toolbar.variant.dense
+            toolbar.children [
+                Html.h1 "Fable.Auth0.React"
+                Mui.button [
+                    button.href "https://github.com/chengh42/Fable.Auth0.React"
+                    button.size.large
+                    button.color.inherit'
+                    prop.children [ Mdi.gitHubIcon [] ]
+                ]
+            ]
+        ]
+    ]
 
 let view (model: Model) (dispatch: Msg -> unit) =
-    Mui.container [
-        container.maxWidth.lg
-        container.children [
-            Html.h1 "Fable.Auth0.React"
-            Html.p [
-                Html.span "Fable library for "
-                Html.a [
-                    prop.href "https://github.com/auth0/auth0-react"
-                    prop.target "_blank"
-                    prop.children [ Html.span "@auth0/auth0-react" ]
+    Html.div [
+        Mui.appBar [
+            appBar.color.primary
+            appBar.position.sticky
+            appBar.children (topBar)
+        ]
+
+        Mui.container [
+            container.maxWidth.lg
+            container.children [
+                Html.p [
+                    Html.span "Fable library for "
+                    Html.a [
+                        prop.href "https://github.com/auth0/auth0-react"
+                        prop.target "_blank"
+                        prop.children [ Html.span "@auth0/auth0-react" ]
+                    ]
+                    Html.span ", the Auth0 SDK for React Single Page Applications (SPA)."
                 ]
-                Html.span ", the Auth0 SDK for React Single Page Applications (SPA)."
-            ]
-            Html.p "Note: Still a work-in-progress!"
-            Mui.button [
-                button.color.primary
-                button.variant.contained
-                prop.text "Hello"
+                Html.p "Note: Still a work-in-progress!"
+                Mui.button [
+                    button.color.primary
+                    button.variant.contained
+                    prop.text "Hello"
+                ]
             ]
         ]
     ]
