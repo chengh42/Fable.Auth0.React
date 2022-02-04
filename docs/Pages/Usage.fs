fd445918a2a84db703e1e77c56660f89ca71fb9b
Hsin <hsin@impactinstitute.com>
2022-02-04 10:52:24 +0100

    use daisyUI

--- docs/Pages/Usage.fs
+++ docs/Pages/Usage.fs
@@ -1,9 +1,10 @@
 [<RequireQualifiedAccess>]
-module BodyContent
+module Pages.Usage
 
+open Fable.Core
+open Fable.Auth0.React
 open Feliz
-
-open CodeSnippet
+open Feliz.DaisyUI
 
 type Snippet =
     static member auth0App = """// JS equivalent: <Auth0Provider/>
@@ -68,37 +69,11 @@ let AuthenticationBox () =
         ]
 """
 
+open CodeSnippet
 
 [<ReactComponent>]
-let InstallationBox () =
+let View () =
     Html.div [
-        Html.h2 "Installation"
-        Html.p "Install npm dependencies:"
-        Highlight.highlight [
-            highlight.language.bash
-            prop.text """# using npm
-npm install @auth0/auth0-react
-
-# using yarn
-yarn add @auth0/auth0-react
-"""
-        ]
-        Html.p "And install nuget package:"
-        Highlight.highlight [
-            highlight.language.bash
-            prop.text """# using nuget
-Install-Package Fable.Auth0.React
-
-# using paket
-paket add Fable.Auth0.React
-"""
-        ]
-    ]
-
-[<ReactComponent>]
-let UsageBox () =
-    Html.div [
-        Html.h2 "Basic usage"
         Html.p [
             Html.span "Before adjusting your F# codebase, configure Auth0 for your React application following the tutorial "
             Html.a [
