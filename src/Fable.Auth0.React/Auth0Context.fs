2cc27a51fe504528bbccc6a5ba5a4bafe07dd9c3
Hsin <hsin@impactinstitute.com>
2022-02-04 17:59:24 +0100

    Add call api; first release (#6)
    * get user metadata
    
    * remove duplicated title
    
    * fix typing for `getAccessTokenSilently`

--- src/Fable.Auth0.React/Auth0Context.fs
+++ src/Fable.Auth0.React/Auth0Context.fs
@@ -31,32 +31,32 @@ type [<AllowNullLiteral>] Auth0ContextInterface<'TUser> =
     /// ```js
     /// const token = await getAccessTokenSilently(options);
     /// ```
-    /// 
+    ///
     /// If there's a valid token stored, return it. Otherwise, opens an
     /// iframe with the `/authorize` URL using the parameters provided
     /// as arguments. Random and secure `state` and `nonce` parameters
     /// will be auto-generated. If the response is successful, results
     /// will be valid according to their expiration times.
-    /// 
+    ///
     /// If refresh tokens are used, the token endpoint is called directly with the
     /// 'refresh_token' grant. If no refresh token is available to make this call,
     /// the SDK falls back to using an iframe to the '/authorize' URL.
-    /// 
+    ///
     /// This method may use a web worker to perform the token call if the in-memory
     /// cache is used.
-    /// 
+    ///
     /// If an `audience` value is given to this function, the SDK always falls
     /// back to using an iframe to make the token exchange.
-    /// 
+    ///
     /// Note that in all cases, falling back to an iframe requires access to
     /// the `auth0` cookie.
     abstract getAccessTokenSilently: Auth0ContextInterfaceGetAccessTokenSilently with get, set
     /// ```js
     /// const token = await getTokenWithPopup(options, config);
     /// ```
-    /// 
+    ///
     /// Get an access token interactively.
-    /// 
+    ///
     /// Opens a popup with the `/authorize` URL using the parameters
     /// provided as arguments. Random and secure `state` and `nonce`
     /// parameters will be auto-generated. If the response is successful,
@@ -65,13 +65,13 @@ type [<AllowNullLiteral>] Auth0ContextInterface<'TUser> =
     /// ```js
     /// const claims = await getIdTokenClaims();
     /// ```
-    /// 
+    ///
     /// Returns all claims from the id_token if available.
     abstract getIdTokenClaims: (GetIdTokenClaimsOptions -> Promise<IdToken option>) with get, set
     /// ```js
     /// await loginWithRedirect(options);
     /// ```
-    /// 
+    ///
     /// Performs a redirect to `/authorize` using the parameters
     /// provided as arguments. Random and secure `state` and `nonce`
     /// parameters will be auto-generated.
@@ -79,12 +79,12 @@ type [<AllowNullLiteral>] Auth0ContextInterface<'TUser> =
     /// ```js
     /// await loginWithPopup(options, config);
     /// ```
-    /// 
+    ///
     /// Opens a popup with the `/authorize` URL using the parameters
     /// provided as arguments. Random and secure `state` and `nonce`
     /// parameters will be auto-generated. If the response is successful,
     /// results will be valid according to their expiration times.
-    /// 
+    ///
     /// IMPORTANT: This method has to be called from an event handler
     /// that was started by the user like a button click, for example,
     /// otherwise the popup will be blocked in most browsers.
@@ -92,7 +92,7 @@ type [<AllowNullLiteral>] Auth0ContextInterface<'TUser> =
     /// ```js
     /// auth0.logout({ returnTo: window.location.origin });
     /// ```
-    /// 
+    ///
     /// Clears the application session and performs a redirect to `/v2/logout`, using
     /// the parameters provided as arguments, to clear the Auth0 session.
     /// If the `federated` option is specified, it also clears the Identity Provider session.
@@ -104,7 +104,7 @@ type [<AllowNullLiteral>] Auth0ContextInterface<'TUser> =
     /// ```js
     /// const authUrl = await buildAuthorizeUrl();
     /// ```
-    /// 
+    ///
     /// Builds an `/authorize` URL for loginWithRedirect using the parameters
     /// provided as arguments. Random and secure `state` and `nonce`
     /// parameters will be auto-generated.
@@ -112,7 +112,7 @@ type [<AllowNullLiteral>] Auth0ContextInterface<'TUser> =
     /// ```js
     /// const logoutUrl = buildLogoutUrl();
     /// ```
-    /// 
+    ///
     /// returns a URL to the logout endpoint using the parameters provided as arguments.
     abstract buildLogoutUrl: (LogoutUrlOptions -> string) with get, set
     /// After the browser redirects back to the callback page,
@@ -121,7 +121,7 @@ type [<AllowNullLiteral>] Auth0ContextInterface<'TUser> =
     /// will be valid according to their expiration times.
     abstract handleRedirectCallback: (string -> Promise<RedirectLoginResult>) with get, set
 
-type [<AllowNullLiteral>] Auth0ContextInterfaceGetAccessTokenSilently =
+type Auth0ContextInterfaceGetAccessTokenSilently =
     [<Emit "$0($1...)">] abstract Invoke: options: obj -> Promise<GetTokenSilentlyVerboseResponse>
-    [<Emit "$0($1...)">] abstract Invoke: ?options: GetTokenSilentlyOptions -> Promise<string>
-    [<Emit "$0($1...)">] abstract Invoke: options: GetTokenSilentlyOptions -> Promise<U2<GetTokenSilentlyVerboseResponse, string>>
+    [<Emit "$0($1...)">] abstract Invoke: options: GetTokenSilentlyOptions -> Promise<string>
+    // [<Emit "$0($1...)">] abstract Invoke: options: GetTokenSilentlyOptions -> Promise<U2<GetTokenSilentlyVerboseResponse, string>>
