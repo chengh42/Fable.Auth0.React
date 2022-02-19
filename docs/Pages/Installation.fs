21c1fb09ff52e81c34d928953991d43d5ca66b63
Hsin <hsin@impactinstitute.com>
2022-02-19 12:18:52 +0100

    rollback build scripts and npm updates

--- docs/Pages/Installation.fs
+++ docs/Pages/Installation.fs
@@ -4,17 +4,30 @@ module Pages.Installation
 open Feliz
 open Shared
 
+let FemtoInstallation () =
+    Html.div [
+        Html.p [
+            Html.span "The easiest way is to use "
+            Html.a ("Femto CLI", "https://github.com/zaid-ajaj/femto")
+            Html.span ", which will take care of all dependencies including npm libraries."
+        ]
+        Highlight.highlight [
+            highlight.language.bash
+            prop.text """femto install Fable.Auth0.React"""
+        ]
+    ]
+
 [<ReactComponent>]
-let View () =
+let ManualInstallation () =
     Html.div [
         Html.p "Install npm dependencies with npm or yarn:"
         Highlight.highlight [
             highlight.language.bash
             prop.text """# using npm
-npm install @auth0/auth0-react
+npm install @auth0/auth0-react@1.9.0
 
 # using yarn
-yarn add @auth0/auth0-react
+yarn add @auth0/auth0-react@1.9.0
 """
         ]
         Html.p "And install nuget package with nuget or paket:"
@@ -27,4 +40,14 @@ Install-Package Fable.Auth0.React
 paket add Fable.Auth0.React
 """
         ]
+    ]
+
+[<ReactComponent>]
+let View () =
+    Html.div [
+        Html.h3 "Using femto"
+        FemtoInstallation ()
+        Html.p [ ]
+        Html.h3 "Manual installation"
+        ManualInstallation ()
     ]
