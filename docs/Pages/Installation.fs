488373ee2088a0f717ffd547a7870e01777ee101
Hsin <chenghsin42@gmail.com>
2022-02-19 15:48:11 +0800

    v1.0.1 femto package versioning constrainst fix (#33)
    * femto installation
    
    * install @auth0/auth0-react@1.9.0
    
    * fix femto dependencies version constraints for @auth0/auth0-react
    
    * rename workflow

--- docs/Pages/Installation.fs
+++ docs/Pages/Installation.fs
@@ -5,16 +5,30 @@ open Feliz
 open Shared
 
 [<ReactComponent>]
-let View () =
+let FemtoInstallation () =
+    Html.div [
+        Html.p [
+            Html.span "The easiest way is to use "
+            Html.a ("Femto CLI", "https://github.com/zaid-ajaj/femto")
+            Html.span ", which will take care of all dependencies including npm libraries."
+        ]
+        Highlight.highlight [
+            highlight.language.bash
+            prop.text """femto install Fable.Auth0.React"""
+        ]
+    ]
+
+[<ReactComponent>]
+let ManualInstallation () =
     Html.div [
         Html.p "Install npm dependencies with npm or yarn:"
         Highlight.highlight [
             highlight.language.bash
             prop.text """# using npm
-npm install @auth0/auth0-react
+npm install @auth0/auth0-react@1.9.0
 
 # using yarn
-yarn add @auth0/auth0-react
+yarn add @auth0/auth0-react@1.9.0
 """
         ]
         Html.p "And install nuget package with nuget or paket:"
@@ -27,4 +41,14 @@ Install-Package Fable.Auth0.React
 paket add Fable.Auth0.React
 """
         ]
+    ]
+
+[<ReactComponent>]
+let View () =
+    Html.div [
+        Html.h3 "Using femto"
+        FemtoInstallation ()
+        Html.p [ ]
+        Html.h3 "Manual installation"
+        ManualInstallation ()
     ]
